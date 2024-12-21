119af200bd4f8b120df8e605ad3a44d0e8de761a
aleksandarMilev <aleksandarmilev23@gmail.com>
2024-12-21 19:45:19 +0200

    Add web and startup projects

---
+++ server/WodItEasy.Web/Features/WorkoutController.cs
@@ -0,0 +1,15 @@
+﻿namespace WodItEasy.Web.Features
+{
+    using System;
+    using Domain.Models.Workouts;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkoutController : ControllerBase
+    {
+        private static readonly Workout Workout = new("Name", "Description", 15, DateTime.Now.AddDays(1), TimeSpan.FromMinutes(60), WorkoutType.CrossFit);
+
+        public ActionResult<Workout> Get() => this.Ok(Workout);
+    }
+}
