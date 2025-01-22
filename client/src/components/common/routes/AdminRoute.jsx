3db6222f53a2460963f17a62dd7eb7ce0581f079
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-01-22 12:49:22 +0200

    Create protected routes

---
+++ client/src/components/common/routes/AdminRoute.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+
+import { UserContext } from '../../../contexts/User'
+import { routes } from '../../../common/constants'
+
+export default function AdminRoute({ element }){
+    const { isAuthenticated, isAdmin } = useContext(UserContext)
+
+    if(!isAuthenticated){
+        return <Navigate to={routes.login} replace />
+    }
+
+    if(!isAdmin){
+        return <Navigate to={routes.accessDenied} replace />
+    }
+
+    return element
+}
---
+++ client/src/components/common/routes/AuthenticatedRoute.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+
+import { UserContext } from '../../../contexts/User'
+import { routes } from '../../../common/constants'
+
+export default function AuthenticatedRoute({ element }){
+    const { isAuthenticated } = useContext(UserContext)
+
+    return isAuthenticated 
+        ? element 
+        : <Navigate to={routes.login} replace />
+}
