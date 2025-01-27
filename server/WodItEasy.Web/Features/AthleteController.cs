dd66d4da54428437454fa4a028545f1e0f0dd691
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-01-27 14:39:32 +0200

    Create Athelte controller

---
+++ server/WodItEasy.Web/Features/AthleteController.cs
@@ -0,0 +1,14 @@
+﻿namespace WodItEasy.Web.Features
+{
+    using System.Threading.Tasks;
+    using Application.Features.Athlete.Commands.Create;
+    using Common;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AthleteController : AuthenticatedApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateAthleteCommand command)
+            => await this.Send(command);
+    }
+}
