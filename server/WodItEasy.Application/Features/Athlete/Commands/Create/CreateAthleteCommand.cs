36f2b6557da9110d855151a34cbb8ba47262a236
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-01-27 14:40:25 +0200

    Implement create athlete command

---
+++ server/WodItEasy.Application/Features/Athlete/Commands/Create/CreateAthleteCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace WodItEasy.Application.Features.Athlete.Commands.Create
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Contracts;
+    using Domain.Factories.Athlete;
+    using MediatR;
+
+    public class CreateAthleteCommand : IRequest<int>
+    {
+        public string Name { get; set; } = null!;
+
+        public class CreateAthleteCommandHandler : IRequestHandler<CreateAthleteCommand, int>
+        {
+            private readonly IAthleteFactory factory;
+            private readonly IAthleteRepository repository;
+            private readonly ICurrentUserService userService;
+
+            public CreateAthleteCommandHandler(
+                IAthleteFactory factory,
+                IAthleteRepository repository,
+                ICurrentUserService userService)
+            {
+                this.factory = factory;
+                this.userService = userService;
+                this.repository = repository;
+            }
+
+            public async Task<int> Handle(CreateAthleteCommand request, CancellationToken cancellationToken)
+            {
+                var athlete = factory
+                    .WithName(request.Name)
+                    .WithUserId(userService.UserId!)
+                    .Build();
+
+                await repository.Save(athlete, cancellationToken);
+
+                return athlete.Id;
+            }
+        }
+    }
+}
