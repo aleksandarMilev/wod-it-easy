8494d059a798d0c29267e42ce885ec29c8f6a40b
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-02-06 09:11:47 +0200

    Implement data seeding

--- server/WodItEasy.Infrastructure/Persistence/WodItEasyInitializer.cs
+++ server/WodItEasy.Infrastructure/Persistence/WodItEasyInitializer.cs
@@ -1,15 +1,65 @@
 ﻿namespace WodItEasy.Infrastructure.Persistence
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Domain.Common;
     using Microsoft.EntityFrameworkCore;
 
     internal class WodItEasyDbInitializer : IInitializer
     {
         private readonly WodItEasyDbContext data;
+        private readonly IEnumerable<IInitialData> initialDataProviders;
 
-        public WodItEasyDbInitializer(WodItEasyDbContext data) 
-            => this.data = data;
+        public WodItEasyDbInitializer(
+            WodItEasyDbContext data,
+            IEnumerable<IInitialData> initialDataProviders)
+        {
+            this.data = data;
+            this.initialDataProviders = initialDataProviders;
+        }
 
-        public void Initialize() 
-            => this.data.Database.Migrate();
+        public void Initialize()
+        {
+            this.data.Database.Migrate();
+
+            foreach (var initialDataProvider in this.initialDataProviders)
+            {
+                if (this.DataSetIsEmpty(initialDataProvider.EntityType))
+                {
+                    var data = initialDataProvider.GetData();
+
+                    foreach (var entity in data)
+                    {
+                        this.data.Add(entity);
+                    }
+                }
+            }
+
+            this.data.SaveChanges();
+        }
+
+        private bool DataSetIsEmpty(Type type)
+        {
+            var setMethod = this.GetType()
+                .GetMethod(nameof(this.GetSet), BindingFlags.Instance | BindingFlags.NonPublic)!
+                .MakeGenericMethod(type);
+
+            var set = setMethod.Invoke(this, []);
+
+            var countMethod = typeof(Queryable)
+                .GetMethods()
+                .First(m => m.Name == nameof(Queryable.Count) && m.GetParameters().Length == 1)
+                .MakeGenericMethod(type);
+
+            var result = (int)countMethod.Invoke(null, [set])!;
+
+            return result == 0;
+        }
+
+        private DbSet<TEntity> GetSet<TEntity>()
+            where TEntity : class
+            => this.data.Set<TEntity>();
     }
 }
