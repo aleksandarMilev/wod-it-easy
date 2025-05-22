e00c2544c7bc8d83af8098b2d7eae3f9b6e7382b
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-05-22 12:46:07 +0300

    use top level statements

--- server/WodItEasy.Startup/Program.cs
+++ server/WodItEasy.Startup/Program.cs
@@ -1,49 +1,38 @@
-namespace WodItEasy.Startup
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using WodItEasy.Application;
+using WodItEasy.Domain;
+using WodItEasy.Infrastructure;
+using WodItEasy.Startup;
+using WodItEasy.Web;
+using WodItEasy.Web.Extensions;
+using WodItEasy.Web.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDomain()
+    .AddApplication(builder.Configuration)
+    .AddInfrastructure(builder.Configuration)
+    .AddWebComponents();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
 {
-    using System.Threading.Tasks;
-    using Application;
-    using Domain;
-    using Infrastructure;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.Extensions.Hosting;
-    using Web;
-    using Web.Extensions;
-    using Web.Middlewares;
-
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services
-                .AddDomain()
-                .AddApplication(builder.Configuration)
-                .AddInfrastructure(builder.Configuration)
-                .AddWebComponents();
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app
-                    .UseSwaggerDocs()
-                    .UseDeveloperExceptionPage();
-            }
-
-            app
-                .UseHttpsRedirection()
-                .UseRouting()
-                .UseValidationExceptionHandler()
-                .UseAllowedCors()
-                .UseAuthentication()
-                .UseAuthorization()
-                .UseAppEndpoints()
-                .Initialize();
-            
-            await app.SeedRoles(builder.Configuration);
-
-            await app.RunAsync();
-        }
-    }
+    app
+        .UseSwaggerDocs()
+        .UseDeveloperExceptionPage();
 }
+
+app
+    .UseHttpsRedirection()
+    .UseRouting()
+    .UseValidationExceptionHandler()
+    .UseAllowedCors()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseAppEndpoints()
+    .Initialize();
+            
+await app.SeedRoles(builder.Configuration);
+await app.RunAsync();
